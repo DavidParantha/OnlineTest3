46d58052c9c3964ea51bb15b85263623fcb9f1a8
David Parantha <150181814+DavidParantha@users.noreply.github.com>
2026-04-23 22:30:41 +0530

    Correct report saving message and error handling
    Fix typo in report saving message and exception handling.

--- Online_Test_3/src/main/java/org/example/ReportGenerator.java
+++ Online_Test_3/src/main/java/org/example/ReportGenerator.java
@@ -6,22 +6,19 @@ import java.util.Map;
 public class ReportGenerator {
 
     public static void generate(Map<String, Integer> data) {
-
         System.out.println("===== USER ACTIVITY REPORT =====");
-
         try {
             FileWriter fw = new FileWriter("report.txt");
             fw.write("===== USER ACTIVITY REPORT =====\n");
-
             for (Map.Entry<String, Integer> entry : data.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue() + " actions");
                 fw.write(entry.getKey() + ": " + entry.getValue() + " actions\n");
             }
-
             fw.close();
             System.out.println("reprot saved to report.txt");
-
         } catch (Exception e) {
+
+            
             System.out.println("faild to write reprot");
         }
 
